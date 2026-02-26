d2245493134927a8f29bc335ecad4be957d6d9d6
Tanupant10 <panttanu04@gmail.com>
2026-02-26 19:17:58 +0530

    Time: 14 ms (50.87%) | Memory: 42 MB (73.33%) - LeetSync

---
+++ 278-first-bad-version/first-bad-version.java
@@ -0,0 +1,20 @@
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int low =0;
+        int high = n-1;
+        
+
+        while (low <= high) {
+            int mid = low + (high-low)/2;
+            if (isBadVersion(mid)){
+                high = mid -1;
+
+            }
+            else {
+                low = mid + 1;
+            }
+        } return low;
+
+        
+    }
+}
