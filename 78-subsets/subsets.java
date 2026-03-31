5f9cdd922c3b69f5b0cc54613906549d20f20bd5
Tanupant10 <panttanu04@gmail.com>
2026-04-01 00:44:56 +0530

    Time: 1 ms (88.26%) | Memory: 44 MB (82.44%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -6,6 +6,8 @@ class Solution {
     }
 
     public void backtrack(int start, List<List<Integer>> sub, List<Integer> temp, int[] nums) {
+        
+        /* 
         sub.add(new ArrayList<>(temp));
 
         for (int i = start; i < nums.length; i++) {
@@ -13,5 +15,20 @@ class Solution {
             backtrack(i + 1, sub, temp, nums);
             temp.remove(temp.size() - 1);
         }
+        */
+
+        if(start == nums.length){
+            sub.add(new ArrayList<>(temp));
+            return ;
+
+        }
+
+        // take
+        temp.add(nums[start]);
+        backtrack(start + 1, sub, temp, nums);
+
+        // not take
+        temp.remove(temp.size() - 1);
+        backtrack(start + 1, sub, temp, nums);
     }
 }
